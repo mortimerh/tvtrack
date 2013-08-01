323264a044275b8f80ba5f6201ce2fdc4aa1d970
Mortimer <mortyh@gmail.com>
2013-08-01 18:54:25 +0200

    ADD Class for season

---
+++ classes/Season.php
@@ -0,0 +1,128 @@
+<?php
+class Season{
+	private $id;
+	private $showId;
+	private $number;
+	private $name;
+	private $tvdbId;
+	private $tvrageId;
+
+	public function __construct($id, $showId, $number, $name, $tvdbId, $tvrageId){
+		$this->id = $id;
+		$this->showId = $showId;
+		$this->number = $number;
+		$this->name = $name;
+		$this->tvdbId = $tvdbId;
+		$this->tvrageId = $tvrageId;
+	}
+
+  /**
+   * Gets the value of id.
+   *
+   * @return mixed
+   */
+  public function getid() {
+    return $this->id;
+  }
+
+  /**
+   * Sets the value of id.
+   *
+   * @param mixed $id the id
+   * @return void
+   */
+  public function setid($id) {
+    $this->id = $id;
+  }
+  /**
+   * Gets the value of showId.
+   *
+   * @return mixed
+   */
+  public function getshowId() {
+    return $this->showId;
+  }
+
+  /**
+   * Sets the value of showId.
+   *
+   * @param mixed $showId the showId
+   * @return void
+   */
+  public function setshowId($showId) {
+    $this->showId = $showId;
+  }
+  /**
+   * Gets the value of number.
+   *
+   * @return mixed
+   */
+  public function getnumber() {
+    return $this->number;
+  }
+
+  /**
+   * Sets the value of number.
+   *
+   * @param mixed $number the number
+   * @return void
+   */
+  public function setnumber($number) {
+    $this->number = $number;
+  }
+  /**
+   * Gets the value of name.
+   *
+   * @return mixed
+   */
+  public function getname() {
+    return $this->name;
+  }
+
+  /**
+   * Sets the value of name.
+   *
+   * @param mixed $name the name
+   * @return void
+   */
+  public function setname($name) {
+    $this->name = $name;
+  }
+  /**
+   * Gets the value of tvdbId.
+   *
+   * @return mixed
+   */
+  public function gettvdbId() {
+    return $this->tvdbId;
+  }
+
+  /**
+   * Sets the value of tvdbId.
+   *
+   * @param mixed $tvdbId the tvdbId
+   * @return void
+   */
+  public function settvdbId($tvdbId) {
+    $this->tvdbId = $tvdbId;
+  }
+  /**
+   * Gets the value of tvrageId.
+   *
+   * @return mixed
+   */
+  public function gettvrageId() {
+    return $this->tvrageId;
+  }
+
+  /**
+   * Sets the value of tvrageId.
+   *
+   * @param mixed $tvrageId the tvrageId
+   * @return void
+   */
+  public function settvrageId($tvrageId) {
+    $this->tvrageId = $tvrageId;
+  }
+}
+?>
